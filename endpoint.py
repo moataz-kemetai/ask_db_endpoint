34d6b72e78aa31e48eab67c4a38c9b70d11a38f1
moataz-kemetai <moataz.mohamed@kemetai.com>
2025-11-01 16:57:43 +0200

    Added connection of db

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+import traceback
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import duckdb
 
 app = FastAPI(title="Egypt API", description="Simple API responding with تحيا مصر")
+DB_PATH = "my_database.duckdb"
 
 class TaskInput(BaseModel):
     query: str
@@ -15,8 +18,29 @@ async def root():
 
 @app.post("/ask_db")
 async def process_task(task: TaskInput):
-    return TaskResponse(message="تحيا مصر")
-
+    try:
+        sql_query = ""
+        print(task.query)
+        con = duckdb.connect(DB_PATH)
+        print("Successfully connected to db.")
+        df = con.execute(task.query).fetchdf()
+        con.close()
+        # Convert to readable string (limit rows)
+        db_response = df.head(1000).to_markdown(index=False)
+        return TaskResponse(message="تحيا مصر")
+    except Exception as e:        
+        raise HTTPException(status_code=400, detail=f"SQL syntax error (Not valid SQL Statement): {e}")
+    except duckdb.CatalogException as e:
+        raise HTTPException(status_code=404, detail=f"Table or column not found: {e}")
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    except Exception as e:
+        # Log full traceback for debugging
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        if con:
+            con.close()
 @app.get("/task")
 async def get_task():
     return {"message": "تحيا مصر"}
